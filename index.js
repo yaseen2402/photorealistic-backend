a79cbc8c6636de7b34c98d03d08c72d833940f35
H-raheel <h.raheel622@gmail.com>
2024-11-09 09:24:24 +0500

    cors

--- index.js
+++ index.js
@@ -4,6 +4,29 @@ const propertyRoutes = require('./routes/propertyRoutes');
 const app = express();
 const PORT = 8080; 
 
+const corsOptions = {
+    origin: '*', // Allow all origins
+    methods: 'GET, POST, PUT, DELETE, OPTIONS',
+    allowedHeaders: 'Authorization,Content-Type',
+    maxAge: 3600
+  };
+  
+  // Middleware
+  app.use(cors(corsOptions));
+  app.options('*', cors(corsOptions)); // Enable preflight OPTIONS request for all routes
+  
+  app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');  
+      res.setHeader('Access-Control-Allow-Methods', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
+      res.setHeader('Access-Control-Max-Age', '3600');
+      // Handle OPTIONS requests
+      if (req.method === 'OPTIONS') {
+        return res.status(204).send('');
+      }
+      next();
+    });
+  
 
 // Hello World endpoint
 app.get('/', (req, res) => {
