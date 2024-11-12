08ee92b7ecede13bc38407cd413f08785ec6f97f
H-raheel <h.raheel622@gmail.com>
2024-11-12 15:43:25 +0500

    testing

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -5,6 +5,6 @@ const { addComment, getComments } = require('../controllers/commentController');
 const router = express.Router();
 
 router.post('/add', (req, res) => addComment(req, res)); 
-router.get('/:zipcode', (req, res) => getComments(req, res)); 
+router.get('/', (req, res) => getComments(req, res)); 
 
 module.exports = router;
