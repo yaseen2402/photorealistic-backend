d8d7874ff106ac8e25d2e02dd25654674968011b
H-raheel <h.raheel622@gmail.com>
2024-11-12 12:12:22 +0500

    comments

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -1,11 +1,11 @@
-// routes/propertyRoutes.js
+
 
 const express = require('express');
 const { getPropertiesByCoordinates } = require('../controllers/propertyController'); // Import the controller function
 
 const router = express.Router();
 
-// Route to get properties by target coordinates
+
 router.get('/', getPropertiesByCoordinates);
 
 module.exports = router;
