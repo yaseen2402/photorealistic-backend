999a305c4cfc218a4cf3658d7448d724071e210f
yaseen <myaseen2402@gmail.com>
2024-11-15 23:21:40 +0530

    restoring options

--- index.js
+++ index.js
@@ -13,32 +13,38 @@ const wellknown = require('wellknown');
 
 
 const corsOptions = {
-  origin: [
-    'http://localhost:3000',
-    'http://localhost:8081/explore',
-    'http://localhost:8081',
-    'exp://localhost:8081',
-    'exp://192.168.*.*:8081',
-    'https://photorealistic-backend-583139081352.us-central1.run.app'
-  ],
+  origin: '*',
   credentials: true,
   methods: 'GET, POST, PUT, DELETE, OPTIONS',
   allowedHeaders: 'Authorization,Content-Type',
-  maxAge: 86400
+  maxAge: 3600
 };
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions)); 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('X-Frame-Options', 'DENY');
-  res.header('X-Content-Type-Options', 'nosniff');
-  res.header('X-XSS-Protection', '1; mode=block');
-  res.header('Content-Security-Policy', "default-src 'self'");
-  res.header('Strict-Transport-Security', 'max-age=31536000');
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', req.headers.origin);
+//   res.header('X-Frame-Options', 'DENY');
+//   res.header('X-Content-Type-Options', 'nosniff');
+//   res.header('X-XSS-Protection', '1; mode=block');
+//   res.header('Content-Security-Policy', "default-src 'self'");
+//   res.header('Strict-Transport-Security', 'max-age=31536000');
   
-  next();
-});
+//   next();
+// });
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');  
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Max-Age', '3600');
+    // Handle OPTIONS requests
+    if (req.method === 'OPTIONS') {
+      return res.status(204).send('');
+    }
+    next();
+  });
 
 
 
