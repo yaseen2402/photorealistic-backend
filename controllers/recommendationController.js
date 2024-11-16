e2ca7e6b10635cf9117eae7fac9f09cf4805725b
Dip Chowdhury <sdipta.c@gmail.com>
2024-11-15 17:54:37 -0700

    found a typo!

--- controllers/recommendationController.js
+++ controllers/recommendationController.js
@@ -156,7 +156,7 @@ const getPropertyRecommendations = async (
         }
 
         console.log(`Final property recommendations: ${propertyRecs.length}`);
-        return filteredZipCodes;
+        return propertyRecs;
     } catch (error) {
         console.error('Error in getPropertyRecommendations:', error);
         throw error;
